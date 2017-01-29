dc3e3a12794f5ea099fdb907b35a0eb153353a9d
Guangling.Hastings <hastings_gl@outlook.com>
2017-01-29 13:40:54 +1100

    Add Route for Pages

--- Planimation2/routes/canvas.js
+++ Planimation2/routes/canvas.js
@@ -1,9 +1,12 @@
+/**
+ * Created by hastings on 29/01/2017.
+ */
 var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index');
+    res.render('canvas');
 });
 
 module.exports = router;
